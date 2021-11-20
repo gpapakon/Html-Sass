dd31bb7132f8ee6a2294118ff4c6ba23fc83bd85
giorgospapakon <papakon1999a@gmail.com>
2021-11-20 18:02:55 +0200

    start

---
+++ My Site/tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "bookmark-purple" : "#5267D7",
+        "bookmark-red" : "#FA5959",
+        "bookmark-blue" : "#242A45",
+        "bookmark-grey" : "#9194A2",
+        "bookmark-white" : "#f7f7f7",
+      },
+    },
+    container: {
+      center:true,
+      padding: "1rem",
+      screens : {
+        lg :"1124px",
+        xl:"1124px",
+        "2xl": "1124",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}

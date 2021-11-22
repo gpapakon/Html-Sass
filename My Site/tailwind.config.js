bb39ef416ab276f5b4f579328472df4bfab778cb
giorgospapakon <papakon1999a@gmail.com>
2021-11-22 20:12:22 +0200

    final

--- My Site/tailwind.config.js
+++ My Site/tailwind.config.js
@@ -7,6 +7,8 @@ module.exports = {
       colors: {
         rose: colors.rose,
         fuchsia: colors.fuchsia,
+        orange: colors.orange,
+        amber: colors.amber,
         "cs-black" : "#211924",
         "cs-uared" : "#E01A4F",
         "cs-silver" : "#EAEAEA",
